3b7da688f384983e300528bfd238b0522ba116f1
ValeriaXZ1 <lesvale568@gmail.com>
2025-03-11 19:01:58 -0500

    Cualquier cosa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,15 @@ import { Pagina404Component } from './pages/pagina404/pagina404.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { FormularioComponent } from './pages/formulario/formulario.component';
 import { ContactosComponent } from './pages/contactos/contactos.component';
+import { loginGuard } from './guards/login.guard';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'nosotros', component: NosotrosComponent },
   { path: 'contactos', component: ContactosComponent },
   { path: 'formulario', component: FormularioComponent },
-  { path: 'productos', component: ProductosComponent },
-  { path: 'productos/tabla/:categoria', component: TablaProductosComponent }, // Ruta dinámica
+  { path: 'productos', component: ProductosComponent, canActivate:[loginGuard]},
+  { path: 'productos/tabla/:categoria', component: TablaProductosComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: Pagina404Component },
 ];

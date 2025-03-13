99cd7e6a358d4dec5f21d736e05d12d40ce583f7
ValeriaXZ1 <lesvale568@gmail>
2025-03-12 22:38:19 -0500

    Roles

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,15 +1,19 @@
+import { Router } from '@angular/router';
 import { CanActivateFn } from '@angular/router';
 
 export const loginGuard: CanActivateFn = (route, state) => {
   const isLoggedIn = localStorage.getItem('login') === 'true';
   const role = localStorage.getItem('role');
+  const router = new Router();
 
   if (isLoggedIn) {
-    // Si la ruta requiere un rol específico
     if (route.data?.['role'] && route.data['role'] !== role) {
+      router.navigate(['/access-denied']); // O redirige donde necesites
       return false;
     }
     return true;
   }
-  return false;
+
+  return false; // No está autenticado
 };
+
